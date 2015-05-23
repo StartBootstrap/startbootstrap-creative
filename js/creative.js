f6449c08738d2f0fadb188f043314395a3aa39a0
Albert Borsos <albertborsos@users.noreply.github.com>
2015-05-23 15:58:16 +0200

    fix: do not close menu if item is a dropdown

--- js/creative.js
+++ js/creative.js
@@ -23,7 +23,7 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
 
