0ccea17c82e03b1b3246bccb3ce2df4907370693
Owen Rogers <exortech@gmail.com>
2016-04-20 15:21:19 -0700

    restore license in generated files

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         banner: '/*!\n' +
             ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
             ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)\n' +
             ' */\n',
         usebanner: {
             dist: {
