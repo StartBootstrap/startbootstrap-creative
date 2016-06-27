60cdf6cad0b1642b5918507b304784a046054697
David Miller <hello@davidmiller.io>
2016-06-27 16:18:45 -0400

    Added a vendor directory to separate third party plugin files from core theme files and updated menu bar responsive toggle

--- js/creative.js
+++ js/creative.js
@@ -1,9 +1,3 @@
-/*!
- * Creative v1.0.4 (http://startbootstrap.com/template-overviews/creative)
- * Copyright 2013-2016 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
- */
-
 (function($) {
     "use strict"; // Start of use strict
 
@@ -26,14 +20,6 @@
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
-
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
 
     // Offset for Main Navigation
     $('#mainNav').affix({
