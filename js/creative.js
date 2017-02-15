de58363635359ec25cf854f4f06e439473b5b713
maxebert <4doom4@users.noreply.github.com>
2017-02-14 20:16:08 -0500

    Fix for bootstrap dropdown menu
    If a dropdown menu is added to the navbar the Responsive Menu is closed by clicking on the dropdown-toggle instead of showing the dropdown-menu. Adding the class page-scroll to the a tag fixes this.

--- js/creative.js
+++ js/creative.js
@@ -17,7 +17,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a.page-scroll').click(function() {
         $('.navbar-toggle:visible').click();
     });
 
