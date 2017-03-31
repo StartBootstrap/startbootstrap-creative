2161defdd58ef2ab03bf0d4cf2c98b1b3b80aeb3
David Miller <hello@davidmiller.io>
2017-03-31 13:30:57 -0400

    revise scroll offset

--- js/creative.js
+++ js/creative.js
@@ -5,7 +5,7 @@
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 48)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
