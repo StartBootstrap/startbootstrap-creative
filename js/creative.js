586e32f925aeaceba83b5f664e25f29a86a2ee09
David Miller <hello@davidmiller.io>
2017-10-20 18:53:11 -0400

    remove popper from gulpfile

--- js/creative.js
+++ js/creative.js
@@ -26,14 +26,18 @@
     offset: 48
   });
 
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
+  // Collapse Navbar
+  var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
     }
-  });
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
   // Scroll reveal calls
   window.sr = ScrollReveal();
