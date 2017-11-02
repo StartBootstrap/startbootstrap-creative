cda1fa39ebf068dd08e2104109584eeb69ec907b
David Miller <hello@davidmiller.io>
2017-11-02 16:18:46 -0400

    bootstrap beta 2 design update

--- js/creative.js
+++ js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
+          scrollTop: (target.offset().top - 57)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 48
+    offset: 57
   });
 
   // Collapse Navbar
