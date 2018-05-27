6486ec27b2d457fada7fdc2a88108a315e0621eb
Timu57 <timucin.uenal@outlook.de>
2018-05-27 17:21:40 +0200

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
+          scrollTop: (target.offset().top - 56)
         }, 1000, "easeInOutExpo");
         return false;
       }
