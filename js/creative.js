3ae844bf1543794b8100bceae82dd0f559d0ef2e
Dan Feeney <dfeeney2456@gmail.com>
2018-05-30 15:27:13 -0500

    day one

--- js/creative.js
+++ js/creative.js
@@ -63,13 +63,63 @@
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
+      enabled: false,
+      // navigateByImgClick: true,
+      // preload: [0, 1]
     },
     image: {
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a href="'+item.el.attr('projurl')+'" target="_blank"> click here </a>';
+			},
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    },
+    // iframe: {
+    //   markup: '<div class="mfp-iframe-scaler">'+
+    //             '<div class="mfp-close"></div>'+
+    //             '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+    //           '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+    
+    //   patterns: {
+    //     youtube: {
+    //       index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+    //       src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+    //     },
+    //     vimeo: {
+    //       index: 'vimeo.com/',
+    //       id: '/',
+    //       src: '//player.vimeo.com/video/%id%?autoplay=1'
+    //     },
+    //     gmaps: {
+    //       index: '//maps.google.',
+    //       src: '%id%&output=embed'
+    //     }
+    
+    //     // you may add here more sources
+    
+    //   },
+    
+    //   srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+    // }
+  });
+
+  $('.video').magnificPopup({
+    type: 'iframe',
+    
+    
+    iframe: {
+       markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    },
+    callbacks: {
+      markupParse: function(template, values, item) {
+       values.title = item.el.attr('title');
+      }
     }
+    
+    
   });
 
 })(jQuery); // End of use strict
