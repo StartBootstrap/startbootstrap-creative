b7bdf916f413fa112d745844b6ace6d64eaa3488
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-15 20:00:04 -0700

    Initial edits

--- js/creative.min.js
+++ js/creative.min.js
@@ -1,6 +1,6 @@
 /*!
  * Start Bootstrap - Creative v5.1.8 (https://startbootstrap.com/template-overviews/creative)
- * Copyright 2013-2019 Start Bootstrap
+ * Copyright 2013-2020 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
  */
 
