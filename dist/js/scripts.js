7636274727ce42a4119cf2f9d5a509921d608b58
David Miller <hello@davidmiller.io>
2020-06-18 16:49:30 -0400

    update dependencies and migration info

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Creative v6.0.2 (https://startbootstrap.com/themes/creative)
+    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
     * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
     (function($) {
   "use strict"; // Start of use strict
