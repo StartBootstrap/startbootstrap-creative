d483b022adff9ae88bb54f10c137a92b5d391188
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-11-02 09:35:45 -0500

    Made some changes to the gh-pages to be able to get my portfolio up and running

---
+++ node_modules/date-fns/esm/locale/fr-CH/_lib/formatLong/index.js
@@ -0,0 +1,10 @@
+import buildFormatLongFn from '../../../_lib/buildFormatLongFn/index.js';
+var formatLong = buildFormatLongFn({
+  LT: 'HH:mm',
+  LTS: 'HH:mm:ss',
+  L: 'DD.MM.YYYY',
+  LL: 'Do MMMM YYYY',
+  LLL: 'Do MMMM YYYY HH:mm',
+  LLLL: 'dddd, Do MMMM YYYY HH:mm'
+});
+export default formatLong;
