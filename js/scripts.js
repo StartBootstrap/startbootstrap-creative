6262492a96121c0a27addbed9467def29b86e1c6
David Miller <hello@davidmiller.io>
2020-11-03 14:51:16 -0500

    Bootstrap 4.5.3 and dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
+    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
