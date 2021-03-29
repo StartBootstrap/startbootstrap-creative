5caf11cc82143aeb32fcaaf0522e0b1be4de3894
David Miller <hello@davidmiller.io>
2021-03-29 19:57:12 -0400

    update smooth scrolling
    jQuery easing is old and deprecated, this new contribution uses animejs instead to update the functionality

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,19 +6,28 @@
     (function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    // Smooth scrolling using anime.js
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 72)
-                }, 1000, "easeInOutExpo");
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top - 72,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
                 return false;
             }
         }
-	});
+    });
 
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
