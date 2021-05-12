71a047703140c362fc442ac733557fec59ee7774
David Miller <hello@davidmiller.io>
2021-05-12 12:59:34 -0400

    add SimpleLightbox plugin

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -51,4 +51,9 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -46,4 +46,9 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
 });
