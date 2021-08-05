cbe0022ed726e3e402d8ac94ec53d64407a9403d
David Miller <hello@davidmiller.io>
2021-08-05 16:02:27 -0400

    Bootstrap 5.1.0 Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Creative v7.0.3 (https://startbootstrap.com/theme/creative)
+* Start Bootstrap - Creative v7.0.4 (https://startbootstrap.com/theme/creative)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
 */
