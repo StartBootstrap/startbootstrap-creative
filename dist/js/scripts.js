937dcc05a3a3d13d0530b4f96c2dee5afcef16d4
David Miller <hello@davidmiller.io>
2022-03-22 16:35:27 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Creative v7.0.5 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
 */
 //
